6386b1ad83745d88315ddf5ae559678dc0d1d370
Everson Alves de Rezende <erezende@osg.com.br>
2024-06-24 09:48:05 -0300

    finish CRUD category

--- SoftFin.Api/Handlers/CategoryHandler.cs
+++ SoftFin.Api/Handlers/CategoryHandler.cs
@@ -1,4 +1,5 @@
-﻿using SoftFin.Api.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SoftFin.Api.Data;
 using SoftFin.Core.Handlers;
 using SoftFin.Core.Models;
 using SoftFin.Core.Requests.Categories;
@@ -8,43 +9,122 @@ namespace SoftFin.Api.Handlers;
 
 public class CategoryHandler(AppDbContext _context) : ICategoryHandler
 {
-    public async Task<Response<Category>> CreateAsync(CreateCategoryRequest request)
+    public async Task<Response<Category?>> CreateAsync(CreateCategoryRequest request)
     {
-        // Instanciar a classe Category
-        var category = new Category
+        try
         {
-            UserId = request.UserId,
-            Title = request.Title,
-            Description = request.Description
-        };
+            // Instanciar a classe Category
+            var category = new Category
+            {
+                UserId = request.UserId,
+                Title = request.Title,
+                Description = request.Description
+            };
 
-        // Adicionar a categoria ao banco de dados
-        await _context.Categories.AddAsync(category);
-        
-        // Salvar as mudanças no banco de dados
-        await _context.SaveChangesAsync();
+            // Adicionar a categoria ao banco de dados
+            await _context.Categories.AddAsync(category);
 
-        // Retornar a categoria criada
-        return new Response<Category>(category);
+            // Salvar as mudanças no banco de dados
+            await _context.SaveChangesAsync();
+
+            // Retornar a categoria criada
+            return new Response<Category?>(category, 201, "Categoria criada com sucesso");
+        }
+        catch
+        {
+            return new Response<Category?>(null, 500, "Não foi possível criar a categoria");
+        }
     }
 
-    public Task<Response<Category>> DeleteAsync(DeleteCategoryRequest request)
+    public async Task<Response<Category?>> DeleteAsync(DeleteCategoryRequest request)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var category = await _context.Categories
+                .FirstOrDefaultAsync(x => x.Id == request.Id && x.UserId == request.UserId);
+
+            if (category is null)
+                return new Response<Category?>(null, 404, "Categoria não encontrada");
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+
+            return new Response<Category?>(category, message: "Categoria excluída com sucesso");
+        }
+        catch
+        {
+            return new Response<Category?>(null, 500, "Não foi possível excluír a categoria");
+        }
     }
 
-    public Task<Response<List<Category>>> GetAllAsync(GetAllCategoriesRequest request)
+    public async Task<PagedResponse<List<Category>>> GetAllAsync(GetAllCategoriesRequest request)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var query = _context
+            .Categories
+            .AsNoTracking()
+            .Where(x => x.UserId == request.UserId)
+            .OrderBy(x => x.Title);
+
+            var categories = await query
+                        .Skip((request.PageNumber - 1) * request.PageSize)
+                        .Take(request.PageSize)
+                        .ToListAsync();
+
+            var count = await query.CountAsync();
+
+            return new PagedResponse<List<Category>>(
+                categories,
+                count,
+                request.PageNumber,
+                request.PageSize);
+        }
+        catch
+        {
+            return new PagedResponse<List<Category>>(null, 500, "Não foi possível consultar as categorias");
+        }
     }
 
-    public Task<Response<Category>> GetByIdAsync(GetCategoryByIdRequest request)
+    public async Task<Response<Category?>> GetByIdAsync(GetCategoryByIdRequest request)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var category = await _context.Categories
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == request.Id && x.UserId == request.UserId);
+
+            return category is null
+                ? new Response<Category?>(null, 404, "Categoria não encontrada")
+                : new Response<Category?>(category);
+        }
+        catch
+        {
+            return new Response<Category?>(null, 500, "Não foi possível recuperar a categoria");
+        }
     }
 
-    public Task<Response<Category>> UpdateAsync(UpdateCategoryRequest request)
+    public async Task<Response<Category?>> UpdateAsync(UpdateCategoryRequest request)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var category = await _context.Categories
+                    .FirstOrDefaultAsync(x => x.Id == request.Id && x.UserId == request.UserId);
+
+            if (category is null)
+                return new Response<Category?>(null, 404, "Categoria não encontrada");
+
+            category.Title = request.Title;
+            category.Description = request.Description;
+
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+
+            return new Response<Category?>(category, message: "Categoria atualizada com sucesso");
+        }
+        catch
+        {
+            return new Response<Category?>(null, 500, "Não foi possível atualizar a categoria");
+        }
     }
 }
--- SoftFin.Api/Program.cs
+++ SoftFin.Api/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using SoftFin.Api.Data;
+using SoftFin.Api.Handlers;
+using SoftFin.Core.Handlers;
 using SoftFin.Core.Models;
 using SoftFin.Core.Requests.Categories;
 using SoftFin.Core.Responses;
@@ -7,7 +9,7 @@ using SoftFin.Core.Responses;
 var builder = WebApplication.CreateBuilder(args);
 var connStr = builder.Configuration.GetConnectionString("DefaultConnection");
 
-builder.Services.AddTransient<Handler>();
+builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(x =>
 {
@@ -24,14 +26,84 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapGet("/", () => "Hello World!");
-
 app.MapPost(
     "/v1/categories",
-    (CreateCategoryRequest request, Handler handler) =>
-    handler.Handle(request))
-    .WithName("Categories: Create")
-    .WithSummary("Cria uma nova categoria")
-    .Produces<Response<Category>>();
+    async (
+        CreateCategoryRequest request,
+        ICategoryHandler handler) =>
+            await handler.CreateAsync(request))
+        .WithName("Categories: Create")
+        .WithSummary("Cria uma nova categoria")
+        .Produces<Response<Category?>>();
+
+app.MapPut(
+    "/v1/categories/{id}",
+    async (
+        long id,
+        UpdateCategoryRequest request,
+        ICategoryHandler handler) =>
+            {
+                request.Id = id;
+                return await handler.UpdateAsync(request);
+            }
+        )
+        .WithName("Categories: Update")
+        .WithSummary("Atualiza uma categoria")
+        .Produces<Response<Category?>>();
+
+app.MapDelete(
+    "/v1/categories/{id}",
+    async (
+        long id,
+        ICategoryHandler handler) =>
+            {
+                var request = new DeleteCategoryRequest
+                {
+                    Id = id,
+                    UserId = "UserID 02"
+
+                };
+                return await handler.DeleteAsync(request);
+            }
+        )
+        .WithName("Categories: Delete")
+        .WithSummary("Exclui uma categoria")
+        .Produces<Response<Category?>>();
+
+app.MapGet(
+    "/v1/categories",
+    async (
+        ICategoryHandler handler) =>
+    {
+        var request = new GetAllCategoriesRequest
+        {
+            UserId = "teste@teste.com"
+
+        };
+        return await handler.GetAllAsync(request);
+    }
+        )
+        .WithName("Categories: Get All")
+        .WithSummary("Recupera todas as categorias de um usuário")
+        .Produces<PagedResponse<List<Category>?>>();
+
+app.MapGet(
+    "/v1/categories/{id}",
+    async (
+        long id,
+        ICategoryHandler handler) =>
+    {
+        var request = new GetCategoryByIdRequest
+        {
+            Id = id,
+            UserId = "teste@teste.com"
+
+        };
+        return await handler.GetByIdAsync(request);
+    }
+        )
+        .WithName("Categories: Get By Id")
+        .WithSummary("Recupera uma categoria")
+        .Produces<Response<Category?>>();
 
 app.Run();
