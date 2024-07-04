bcb948b9dd73b5cc4ef55b323063eedf5c0034c9
Everson Alves de Rezende <erezende@osg.com.br>
2024-07-04 17:34:11 -0300

    resolve dependencies

---
+++ SoftFin.Web/Pages/Identity/Register.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace SoftFin.Web.Pages.Identity;
+
+public partial class RegisterPage : ComponentBase
+{
+    public MudForm? MudForm { get; set; }
+}
--- SoftFin.Web/Program.cs
+++ SoftFin.Web/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -5,10 +6,20 @@ using SoftFin.Web;
 using SoftFin.Web.Security;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+Configuration.BackendUrl = builder.Configuration.GetValue<string>("BackendUrl") ?? string.Empty;
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped<CookieHandler>();
+
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddScoped<AuthenticationStateProvider, CookieAuthenticationStateProvider>();
+
+builder.Services.AddScoped(x => (ICookieAuthenticationStateProvider)x.GetRequiredService<AuthenticationStateProvider>());
+
 builder.Services.AddMudServices();
 builder.Services.AddHttpClient(Configuration.HttpClientName, opt =>
 {
